92afc5d66b10ffaa7c88b54c7536ad71af00f2b1
Izar <izar.decleto@gmail.com>
2019-06-09 00:08:36 -0700

    Following a/A

--- board.rb
+++ board.rb
@@ -4,22 +4,7 @@ class Board
     attr_reader :mines
     def initialize
         #arranges the grid, still need to figure out the tiles
-        @grid = Array.new(9) {Array.new(9){make_mines} }
-        
+        @grid = Array.new(9) {Array.new(9){make_mines.rand} }
     end 
-
-    def make_mines
-        #clean this up
-        @mines = ['* ', '  '].sample
-    end
-
-    def print_board
-        puts "Welcome to Izar's Mine Sweeper"
-        @grid.each_with_index do |row, i|
-            print "#{i + 1}   " if i < 9
-            print "#{i + 1}  " if i >= 9
-        end
-
-    end
 
 end
--- tile.rb
+++ tile.rb
@@ -1,16 +1,45 @@
 class Tile
 
-    def initialize()
-        @tile = tile
-    end
+    DELTAS = [
+        [-1,-1],
+        [-1, 0],
+        [-1, 1], 
+        [0, -1], 
+        [0, 1], 
+        [1, -1], 
+        [1, 0], 
+        [1, 1]
+    ].freeze
 
-    def revealed?
+    attr_reader :pos
+
+    def initialize(board, pos)
+        @board, @pos = board, pos
+        @bombed, @explored, @flagged = false, false, false
     end
 
     def bombed?
+        @bombed
+    end
+
+    def explored?
+        @explored
     end
 
     def flagged?
+        @flagged
     end
+
+    # def neighbors
+    #     adjacent_coords = DELTAS.map do |(dx, dy)|
+    #         [pos[0] + dx, pos[1] + dy]
+    #     end.select do |row, col|
+    #         [row, col].all? do |coord|
+    #             coord.between?(0, @board.grid_size - 1)
+    #         end
+    #     end
+
+    #     adjacent_coords.map { |pos| @board[pos]}
+    # end
 
 end
